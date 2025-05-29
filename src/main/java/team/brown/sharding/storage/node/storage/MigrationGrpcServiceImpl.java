a270d09351f2e7eb81b7f72068b644c15806f5f8
Elizaveta Ilina <el.e.ilyina@tbank.ru>
2025-05-29 23:03:51 +0300

    added chunks in grpc

--- src/main/java/team/brown/sharding/storage/node/storage/MigrationGrpcServiceImpl.java
+++ src/main/java/team/brown/sharding/storage/node/storage/MigrationGrpcServiceImpl.java
@@ -4,12 +4,13 @@ import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import team.brown.sharding.storage.proto.MigrateDataRequest;
+import team.brown.sharding.storage.proto.MigrateDataChunk;
 import team.brown.sharding.storage.proto.MigrateDataResponse;
 import team.brown.sharding.storage.proto.MigrationGrpcServiceGrpc;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -20,24 +21,38 @@ public class MigrationGrpcServiceImpl extends MigrationGrpcServiceGrpc.Migration
     private final StorageService storageService;
 
     @Override
-    public void migrateData(MigrateDataRequest request, StreamObserver<MigrateDataResponse> responseObserver) {
-        log.info("Received migration request: keys={}", request.getDataMap().keySet());
-        try {
-            log.info("Get new data by migration: {}", request.getDataMap());
+    public StreamObserver<MigrateDataChunk> migrateData(StreamObserver<MigrateDataResponse> responseObserver) {
+        Map<String, String> allData = new HashMap<>();
+        final int[] version = new int[1];
 
-            storageService.putAll(request.getDataMap());
-            storageService.updateVersion(request.getVersion());
-            log.info("Migration completed successfully");
-            responseObserver.onNext(MigrateDataResponse.newBuilder()
-                    .setSuccess(true)
-                    .build());
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            log.error("Migration failed: error={}", e.getMessage());
-            responseObserver.onNext(MigrateDataResponse.newBuilder()
-                    .setSuccess(false)
-                    .build());
-            responseObserver.onCompleted();
-        }
+        return new StreamObserver<>() {
+            @Override
+            public void onNext(MigrateDataChunk chunk) {
+                log.info("Received chunk with {} keys", chunk.getDataMap().size());
+                allData.putAll(chunk.getDataMap());
+                version[0] = chunk.getVersion();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.error("Migration stream error: {}", t.getMessage(), t);
+            }
+
+            @Override
+            public void onCompleted() {
+                try {
+                    log.info("Final assembled data size: {}", allData.size());
+                    storageService.putAll(allData);
+                    storageService.updateVersion(version[0]);
+                    log.info("Migration completed successfully");
+                    responseObserver.onNext(MigrateDataResponse.newBuilder().setSuccess(true).build());
+                    responseObserver.onCompleted();
+                } catch (Exception e) {
+                    log.error("Migration failed during final write: {}", e.getMessage(), e);
+                    responseObserver.onNext(MigrateDataResponse.newBuilder().setSuccess(false).build());
+                    responseObserver.onCompleted();
+                }
+            }
+        };
     }
 }
